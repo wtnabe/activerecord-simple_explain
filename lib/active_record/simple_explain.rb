c4246b4ab538676bcbf8393b1b53fb86f11ff581
wtnabe <wtnabe@gmail.com>
2013-09-20 22:17:40 +0900

    initial commit

---
+++ lib/active_record/simple_explain.rb
@@ -0,0 +1,7 @@
+require "active_record/simple_explain/version"
+
+module ActiveRecord
+  module SimpleExplain
+    # Your code goes here...
+  end
+end
---
+++ lib/active_record/simple_explain/version.rb
@@ -0,0 +1,5 @@
+module ActiveRecord
+  module SimpleExplain
+    VERSION = "0.0.1"
+  end
+end
